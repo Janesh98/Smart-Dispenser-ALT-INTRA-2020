051e6805e46b70c2a41252d8f28e478750624e75
Janesh Sharma <janesh.sharma3@mail.dcu.ie>
2020-06-19 14:46:12 +0100

    add total dispensals

--- code/data_analysis/data_analysis.py
+++ code/data_analysis/data_analysis.py
@@ -8,7 +8,9 @@ def get_data(url):
 
 def compute_statistics(dispensers, dispenser_data):
     overall_ignored = []
+    overall_dispensals = []
     for dispenser in dispensers:
+        dispensals = []
         ignored = []
         name = dispenser["name"]
         location = dispenser["location"]
@@ -24,15 +26,28 @@ def compute_statistics(dispensers, dispenser_data):
         # % dispenser was not ignored
         adherence_rate = 1 - ignored_rate
 
+        # number of times dispenser was used
+        # False ignore entries mean a dispensal occurred
+        # dispensal count is the sum of False entries
+        dispensal_count = len(ignored) - sum(ignored)
+
+        if len(overall_dispensals) > 1:
+            overall_dispensals.append(dispensal_count)
+        else:
+            overall_dispensals.append(dispensal_count)
+
         overall_ignored.append(ignored_rate)
 
-        print("{}\nlocation: {}\ncreated: {}\ndevice_id: {:d}\nIgnored rate: {:.2f}%\nAdherence rate: {:.2f}%\n".format(name, location, created, id_1, ignored_rate, adherence_rate))
+        print("{}\nLocation: {}\nCreated: {}\nDevice_id: {:d}\nIgnored rate: {:.2f}%\nAdherence rate: {:.2f}%\nTotal dispensals: {:d}\n".format(name, location, created, id_1, ignored_rate, adherence_rate, dispensal_count))
 
     overall_ignored_rate = sum(overall_ignored) / len(overall_ignored)
     overall_adherance_rate = 1 - overall_ignored_rate
 
+    overall_total_dispensals = sum(overall_dispensals)
+
     print("Overall ignored rate: {:.2f}%".format(overall_ignored_rate))
     print("Overall adherance rate: {:.2f}%".format(overall_adherance_rate))
+    print("Overall total dispensals: {:d}".format(overall_total_dispensals))
 
 def main():
     dispensers = get_data("dispenser/all")
