e21de1e4f45ca3d28e53a4948034e2eca91cdaf4
Janesh Sharma <janesh.sharma3@mail.dcu.ie>
2020-06-23 23:15:25 +0100

    use better terminology

--- code/data_analysis/data_analysis.py
+++ code/data_analysis/data_analysis.py
@@ -9,7 +9,7 @@ def get_data(url):
 
 def compute_statistics(dispensers, dispenser_data):
     overall_ignored = []
-    overall_dispensals = []
+    overall_dispenses = []
     for dispenser in dispensers:
         ignored = []
         name = dispenser["name"]
@@ -32,40 +32,40 @@ def compute_statistics(dispensers, dispenser_data):
         dispensal_count = len(ignored) - sum(ignored)
 
         # append everything from one list to another
-        overall_dispensals.extend(ignored)
+        overall_dispenses.extend(ignored)
 
         overall_ignored.append(ignored_rate)
 
-        print("{}\nLocation: {}\nCreated: {}\nDevice_id: {:d}\nIgnored rate: {:.2f}%\nAdherence rate: {:.2f}%\nTotal dispensals: {:d}\n".format(name, location, created, id_1, ignored_rate, adherence_rate, dispensal_count))
+        print("{}\nLocation: {}\nCreated: {}\nDevice_id: {:d}\nIgnored rate: {:.2f}%\nAdherence rate: {:.2f}%\nTotal dispenses: {:d}\n".format(name, location, created, id_1, ignored_rate, adherence_rate, dispensal_count))
 
     overall_ignored_rate = sum(overall_ignored) / len(overall_ignored)
-    overall_adherance_rate = 1 - overall_ignored_rate
+    overall_adherence_rate = 1 - overall_ignored_rate
 
-    overall_total_dispensals = sum(overall_dispensals)
+    overall_total_dispenses = sum(overall_dispenses)
 
     print("Overall ignored rate: {:.2f}%".format(overall_ignored_rate))
-    print("Overall adherance rate: {:.2f}%".format(overall_adherance_rate))
-    print("Overall total dispensals: {:d}".format(overall_total_dispensals))
+    print("Overall adherence rate: {:.2f}%".format(overall_adherence_rate))
+    print("Overall total dispenses: {:d}".format(overall_total_dispenses))
 
-    plot_dispensals(overall_dispensals)
+    plot_dispenses(overall_dispenses)
 
-def plot_dispensals(dispensals):
+def plot_dispenses(dispenses):
     # convert True to 1 and False to 0
-    for i in range(len(dispensals)):
-        if dispensals[i] == True:
-            dispensals[i] = 1
+    for i in range(len(dispenses)):
+        if dispenses[i] == True:
+            dispenses[i] = 1
         else:
-            dispensals[i] = 0
+            dispenses[i] = 0
 
-    # convert list to show the total dispensals so far for each time period
+    # convert list to show the total dispenses so far for each time period
     # e.g [0, 1, 1, 0] becomes [0, 1, 2, 2]
-    for i in range(1, len(dispensals)):
-        dispensals[i] += dispensals[i-1]
+    for i in range(1, len(dispenses)):
+        dispenses[i] += dispenses[i-1]
 
-    plt.title("Total dispensals over time")
+    plt.title("Total dispenses over time")
     plt.xlabel("Time")
-    plt.ylabel("Dispensals")
-    plt.plot(dispensals, label="Dispensals")
+    plt.ylabel("Dispenses")
+    plt.plot(dispenses, label="Dispenses")
     plt.grid()
     plt.legend()
     plt.show()
